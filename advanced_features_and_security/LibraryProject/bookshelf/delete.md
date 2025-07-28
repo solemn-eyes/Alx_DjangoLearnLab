d914d1673449295c0066a04348c5bbcdbfeca412
Jerome Maunga <jerhomemaunga@gmail.com>
2025-07-28 10:08:10 +0300

    The new repo

---
+++ advanced_features_and_security/LibraryProject/bookshelf/delete.md
@@ -0,0 +1,4 @@
+from bookshelf.models import Book
+book.delete()
+Book.objects.all()
+
---
+++ advanced_features_and_security/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,21 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+def get_books_by_author(author_id):
+    return Book.objects.filter(author_id=author_id)
+
+# List all books in a library
+def get_books_in_library(library_id):
+    return Book.objects.filter(library_id=library_id)
+
+Library.objects.get(name=library_name)
+
+
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_id):
+    try:
+        library = Library.objects.get(id=library_id)
+        return library.librarian  # Assuming a OneToOneField or ForeignKey to Librarian
+    except Library.DoesNotExist:
+        return None
+    
