2cd5a806b17b04157c0048adfdcc67080f79f83b
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-03 19:59:22 +0300

    Implementing Authentication in REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
