818d0df0c04e45de0d0c508d093613a2ffdb4a73
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-07 18:39:55 +0300

    Custom views and Generic views in DRF

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import MyAPIView
 
 urlpatterns = [
-    path('my-api/', MyAPIView.as_view(), name='my_api'),
-    path('my-api/<int:id>/', MyAPIView.as_view(), name='my_api_detail'),
+    path('books', 'book/create/', name='book_create'),
+    path('books/<int:book_id>/', 'book/detail/', name='book_detail'),
+    path('books/<int:book_id>/update/', 'book/update/', name='book_update'),
+    path('books/<int:book_id>/delete/', 'book/delete/', name='book_delete'),
+    path('my-api/', MyAPIView.as_view(), name='my_api'),  
 ]
