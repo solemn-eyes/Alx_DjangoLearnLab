3931fbcc95c52aab71d54b168f5db1e3c48b5e98
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-07 13:16:07 +0300

    Using Serializers in DRF

---
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Author, Book
+import datetime
+
+# Serializers for Book models
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title', 'publication_year', 'author']
+    def validate_publication_year(self, value):
+        current_year = datetime.date.today().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+# Serializer for Author model
+class AuthorSerializer(serializers.ModelSerializer):
+    books = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    books = BookSerializer(many=True, read_only=True)
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
